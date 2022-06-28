3441756120faf6f6ec2d74ba93b3d8d7dd76c864
Robert Schröder <r.schroeder@agila-online.de>
2022-06-28 12:47:20 +0200

    changed gpio lib in gpio device

--- device/Pigpio.js
+++ device/Pigpio.js
@@ -1,4 +1,4 @@
-import { Gpio } from 'pigpio'
+import { pigpio } from 'pigpio-client'
 import Device from './base.js'
 
 const LIGHT_BRIGHTNESS_SCALE = 255
@@ -14,64 +14,83 @@ export default class extends Device {
   }
 
   async connect () {
-    this.config.pins.forEach(pin => {
-      const key = 'pin_' + pin.id
-      const type = pin.type || 'in'
-      const entityTypes = {
-        in: 'binary_sensor',
-        out: 'switch',
-        pwm: 'light'
-      }
-      const entity = {
-        name: pin.name,
-        key,
-        type: entityTypes[type]
-      }
-      const emit = state => {
-        entity.states = {
-          state: (state === 1 && !pin.inverted) || (state !== 1 && pin.inverted) ? 'ON' : 'OFF'
-        }
-
-        this.emitEntity(entity)
-      }
-      const emitLight = state => {
-        entity.brightness = true
-        entity.brightnessScale = LIGHT_BRIGHTNESS_SCALE
-        entity.states = {
-          state: state ? '{"state": "ON", "brightness": ' + state + '}' : '{"state": "OFF"}'
-        }
-
-        this.emitEntity(entity)
-      }
-
-      if (type === 'out') {
-        INSTANCES[key] = new Gpio(pin.id, { mode: Gpio.OUTPUT, alert: true })
-        INSTANCES[key].on('alert', emit)
-        INSTANCES[key].digitalWrite(pin.inverted ? 1 : 0)
-
-        entity.commands = ['command']
-
-        emit(pin.inverted ? 1 : 0)
-      }
-
-      if (type === 'pwm') {
-        INSTANCES[key] = new Gpio(pin.id, { mode: Gpio.OUTPUT, alert: true })
-        INSTANCES[key].pwmWrite(0)
-        INSTANCES[key].on('pwm_change', () => emitLight(INSTANCES[key].getPwmDutyCycle()))
-
-        entity.commands = ['command']
-        
-        emitLight(0)
-      }
-
-      if (type === 'in') {
-        INSTANCES[key] = new Gpio(pin.id, { mode: Gpio.INPUT, alert: true })
-        INSTANCES[key].on('alert', emit)
-        INSTANCES[key].glitchFilter(pin.debounce || 50000)
-
-        emit(INSTANCES[key].digitalRead())
-      }
+    const client = pigpio({ host: 'localhost' })
+    const ready = new Promise((resolve, reject) => {
+      client.once('connected', resolve)
+      client.once('error', reject)
     })
+
+    ready.then(async (info) => {
+      this.config.pins.forEach(pin => {
+        const key = 'pin_' + pin.id
+        const type = pin.type || 'in'
+        const entityTypes = {
+          in: 'binary_sensor',
+          out: 'switch',
+          pwm: 'light'
+        }
+        const entity = {
+          name: pin.name,
+          key,
+          type: entityTypes[type]
+        }
+        const emit = state => {
+          entity.states = {
+            state: (state === 1 && !pin.inverted) || (state !== 1 && pin.inverted) ? 'ON' : 'OFF'
+          }
+
+          this.emitEntity(entity)
+        }
+
+        if (type === 'out') {
+          INSTANCES[key] = client.gpio(pin.id)
+          INSTANCES[key].modeSet('output')
+          INSTANCES[key].notify(emit)
+          INSTANCES[key].write(pin.inverted ? 1 : 0)
+
+          entity.commands = ['command']
+
+          emit(pin.inverted ? 1 : 0)
+        }
+
+        if (type === 'pwm') {
+          INSTANCES[key] = client.gpio(pin.id)
+          INSTANCES[key].modeSet('output')
+          INSTANCES[key].analogWrite(0)
+          INSTANCES[key].emit = state => {
+            if (state.brightness === undefined) {
+              state.brightness = LIGHT_BRIGHTNESS_SCALE
+            }
+
+            entity.brightness = true
+            entity.brightnessScale = LIGHT_BRIGHTNESS_SCALE
+            entity.states = {
+              state: JSON.stringify(state)
+            }
+  
+            this.emitEntity(entity)
+          }
+
+          entity.commands = ['command']
+          
+          INSTANCES[key].emit({ state: 'OFF' })
+        }
+
+        if (type === 'in') {
+          INSTANCES[key] = client.gpio(pin.id)
+          INSTANCES[key].modeSet('input')
+          INSTANCES[key].notify(emit)
+          INSTANCES[key].glitchSet(pin.debounce || 50000)
+          INSTANCES[key].read((error, state) => {
+            if (error) {
+              console.error(error)
+            } else {
+              emit(state)
+            }
+          })
+        }
+      })
+    }).catch(console.error)
   }
 
   async handle (key, state, value) {
@@ -81,11 +100,11 @@ export default class extends Device {
     if (INSTANCES[key] && pin) {
       if (pin.type === 'pwm') {
         value = JSON.parse(value)
-        console.log(value)
-        INSTANCES[key].pwmWrite(value.state === 'ON' ? value.brightness || LIGHT_BRIGHTNESS_SCALE : 0)
-        INSTANCES[key].emit('pwm_change')
+
+        INSTANCES[key].analogWrite(value.state === 'ON' ? value.brightness || LIGHT_BRIGHTNESS_SCALE : 0)
+        INSTANCES[key].emit(value)
       } else {
-        INSTANCES[key].digitalWrite((value === 'ON' && !pin.inverted) || (value !== 'ON' && pin.inverted) ? 1 : 0)
+        INSTANCES[key].write((value === 'ON' && !pin.inverted) || (value !== 'ON' && pin.inverted) ? 1 : 0)
       }
     }
   }
