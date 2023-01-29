53f8e9df37f7a67ef6fbec3d1a8f6ee3c01ecdb4
Rob <gitlab@s-r.cc>
2023-01-29 16:53:16 +0100

    optimized pulseaudio device

--- device/Pulseaudio.js
+++ device/Pulseaudio.js
@@ -1,6 +1,7 @@
 import Device from './base.js'
-import { exec } from 'child_process'
-import { PulseAudio, percentToVolume, volumeToPercent } from 'pulseaudio.js'
+import { PulseAudio, percentToVolume, volumeToPercent, dBToVolume, volumeTodB } from 'pulseaudio.js'
+
+const DB_MIN = -130
 
 export default class extends Device {
   constructor (config) {
@@ -18,64 +19,97 @@ export default class extends Device {
     await this.client.connect()
     
     this.poll('getSinks', 5000)
-     
   }
 
   async getSinks () {
     const server = await this.client.getServerInfo()
     const sinks = await this.client.getAllSinks()
+    const options = {}
 
     sinks.forEach(sink => {
-      this.emitEntity({
-        type: 'switch',
-        name: sink.description,
-        key: 'sink_' + sink.name.replace(/\W/g, '_'),
-        commands: ['command'],
-        states: {
-          state: server.defaultSink === sink.name ? 'ON' : 'OFF'
-        }
-      })
+      options[sink.name] = sink.description
 
-      if (server.defaultSink === sink.name) { 
-        this.emitVolume(Math.round(sink.volume.current[0] / (sink.volume.steps - 1) * 100), !!sink.mute)
+      if (server.defaultSink === sink.name) {
+        const db = volumeTodB(sink.volume.current[0]).toFixed(1)
+
+        this.emitVolumeDb(db < DB_MIN ? DB_MIN : db)
+        this.emitVolumePercent(volumeToPercent(sink.volume.current[0]).toFixed(0))
+        this.emitMute(sink.mute ? 'ON' : 'OFF')
       }
+    })
+
+    this.emitEntity({
+      type: 'select',
+      name: 'Sink',
+      key: 'sink',
+      options,
+      commands: ['command'],
+      states: { state: options[server.defaultSink] }
     })
   }
 
-  emitVolume (level, mute) {
+  emitVolumePercent (state) {
     this.emitEntity({
-      type: 'light',
-      name: 'Volume',
-      key: 'volume',
-      brightness: true,
-      brightnessScale: 100,
+      type: 'number',
+      name: 'Volume percent',
+      key: 'volume_percent',
+      min: 0,
+      max: 100,
+      step: 1,
       commands: ['command'],
-      states: {
-        state: JSON.stringify({
-          state: mute ? 'OFF' : 'ON',
-          brightness: level
-        })
-      }
+      states: { state }
+    })
+  }
+
+  emitVolumeDb (state) {
+    this.emitEntity({
+      type: 'number',
+      name: 'Volume dB',
+      key: 'volume_db',
+      min: DB_MIN,
+      max: 0,
+      step: 0.1,
+      class: 'sound_pressure',
+      unit: 'dB',
+      commands: ['command'],
+      states: { state }
+    })
+  }
+
+  emitMute (state) {
+    this.emitEntity({
+      type: 'switch',
+      name: 'Mute',
+      key: 'mute',
+      commands: ['command'],
+      states: { state }
     })
   }
 
   async handle (control, state, value) {
-    if (control === 'volume') {
-      value = JSON.parse(value)
-
-      this.client.setSinkMute(value.state === 'OFF')
-
-      if (value.brightness) {
-        this.client.setSinkVolume(percentToVolume(value.brightness))
-      }
-
-      this.emitVolume(value.brightness, value.state === 'OFF')
-    } else if (control.slice(0, 5) === 'sink_') {
+    if (control === 'volume_percent') {
+      this.client.setSinkVolume(percentToVolume(value))
+      
+      this.emitVolumePercent(value)
+    } else if (control === 'volume_db') {
+      if (value <= DB_MIN) value = -Infinity
+      this.log('SET', value)
+      this.client.setSinkVolume(dBToVolume(value))
+      
+      this.emitVolumePercent(value)
+    } else if (control === 'mute') {
+      this.client.setSinkMute(value === 'ON')
+      
+      this.emitMute(value)
+    } else if (control === 'sink') {
       const sinks = await this.client.getAllSinks()
-      const sink = sinks.find(sink => control.slice(5) === sink.name.replace(/\W/g, '_'))
+      const sink = sinks.find(sink => sink.name === value)
 
-      this.client.setDefaultSink(sink.name)
-      this.getSinks()
+      if (sink) {
+        this.client.setDefaultSink(sink.name)
+      }
     }
+
+    this.getSinks()
   }
 }
