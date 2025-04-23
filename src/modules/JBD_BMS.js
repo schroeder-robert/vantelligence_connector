0e6d3cc21fb85f79d50fdf79613ef6d883c22aeb
Robert Schröder <robert.schroeder@agila.de>
2025-04-23 16:30:39 +0200

    updated jbd bms code

--- src/modules/JBD_BMS.js
+++ src/modules/JBD_BMS.js
@@ -24,7 +24,6 @@ const ERRORS = {
   frontend_ic_error: 'Frontend IC error',
   fet_locked: 'Charge or Discharge FET locked by config (See register 0xE1 "MOSFET control")'
 }
-const ERROR_PROCESS_INFO_MESSAGE = 'Cannot process info message: '
 const ERROR_REGISTER_RESPONSE = 'Response error for register: '
 const ERROR_WRONG_RESPONSE_REGISTER = 'Wrong response register: '
 const ERROR_BUFFER_TOO_SHORT = 'Buffer too short'
@@ -50,53 +49,82 @@ export default async ({ device, poll, prop, createSerialConnection, entityClasse
   const cycleCount = dev.sensor('cycle_count', 'Ladezyklen', { category: entityCategories.diagnostic })
   const cellCount = dev.sensor('cell_count', 'Anzahl Zellen', { category: entityCategories.diagnostic })
   const temperatureSensorCount = dev.sensor('temperature_sensor_count', 'Anzahl Temperatursensoren', { category: entityCategories.diagnostic })
-  const errors = Object.keys(ERRORS).map(id => dev.binarySensor(id, ERRORS[id], { category: entityCategories.diagnostic }))
-  
+  const errors = Array.from({ length: 16 }).map(id => dev.binarySensor(id, ERRORS[id], { category: entityCategories.diagnostic }))
+  const temperatures = []
+  const cells = []
+
   // communication
   const sendSerial = createSerialConnection({ path: connection.port, baudRate: 9600 })
   const read = (key, data) => send(sendSerial, READ_BYTE, key, data)
   const write = (key, data) => send(sendSerial, WRITE_BYTE, key, data)
-  const info = decodeMessage('info', await read('info'))
+  const requestName = async () => {
+    const buffer = await read('name')
+
+    if (buffer.length < 1) return logError(ERROR_BUFFER_TOO_SHORT, 'Expected: 1', 'Got: ' + buffer.length)
+
+    name.state(buffer.toString())
+  }
+  const requestBasics = async () => {
+    const buffer = await read('info')
+
+    if (buffer.length < 23) return logError(ERROR_BUFFER_TOO_SHORT, 'Expected: 23', 'Got: ' + buffer.length)
+
+    manufactureDate.state((d => [(d >> 9) + 2000, ((d >> 5) & 15).toString(10).padStart(2, '0'), (d & 31).toString(10).padStart(2, '0')].join('-'))(buffer.readUInt16BE(10)))
+    softwareVersion.state('0x' + buffer.readUInt8(18).toString(16))
+    cellCount.state(buffer.readUInt8(21))
+    temperatureSensorCount.state(buffer.readUInt8(22))
+  }
+  const requestInfo = async () => {
+    const buffer = await read('info')
+
+    if (buffer.length < 23) return logError(ERROR_BUFFER_TOO_SHORT, 'Expected: 23', 'Got: ' + buffer.length)
+
+    voltage.state(Math.round(filters.packVoltage.filter(buffer.readUInt16BE(0))) / 100)
+    current.state(Math.round(filters.packCurrent.filter(buffer.readInt16BE(2))) / 100)
+    cycleCapacity.state(buffer.readUInt16BE(4) / 100)
+    designCapacity.state(buffer.readUInt16BE(6) / 100)
+    cycleCount.state(buffer.readUInt16BE(8))
+    errors.forEach((e, i) => e.state((buffer.readUInt16BE(16) >> i) & 1 ? stateValues.off : stateValues.on))
+    stateOfCharge.state(buffer.readUInt8(19))
+    chargeEnabled.state(buffer.readUInt8(20) & 1 ? stateValues.on : stateValues.off)
+    dischargeEnabled.state(buffer.readUInt8(20) >> 1 ? stateValues.on : stateValues.off)
+    cells.forEach((c, i) => c.balance.state(((i < 16 ? buffer.readUInt16BE(12) : buffer.readUInt16BE(14)) >> i%16) & 1 ? stateValues.on : stateValues.off))
+    temperatures.forEach((t, i) => t.state((buffer.readUInt16BE(23 + (i * 2)) - 2731) / 10))
+  }
+  const requestVoltages = async () => {
+    const buffer = await read('info')
+    const length = cellCount.state() * 2
+
+    if (buffer.length < length) return logError(ERROR_BUFFER_TOO_SHORT, 'Expected: ' + length, 'Got: ' + buffer.length)
+
+    cells.forEach((c, i) => c.voltage.state(buffer.readUInt16BE(i * 2) / 1000))
+  }
+
+  await requestName()
+  await requestBasics()
 
   // result dependent entities
-  const temperatures = Array.from({ length: info.ntcCount }).map((v, i) => dev.sensor('temperatute' + i, 'Temperatur #' + (i + 1), { category: entityCategories.diagnostic, class: entityClasses.temperature, unit: entityUnits.celsius }))
-  const cells = Array.from({ length: info.packCells }).map((v, i) => ({
-    voltage: dev.sensor('cell' + i + '_voltage', 'Zelle ' + (i + 1) + ' Spannung', {
-      category: entityCategories.diagnostic,
-      class: entityClasses.voltage,
-      unit: entityUnits.volt
-    }),
-    balance: dev.binarySensor('cell' + i + '_balance', 'Zelle ' + (i + 1) + ' Balance', {
-      category: entityCategories.diagnostic,
+  for (let i = 0; i < temperatureSensorCount.state(); ++i) {
+    temperatures.push(dev.sensor('temperatute' + i, 'Temperatur #' + (i + 1), { category: entityCategories.diagnostic, class: entityClasses.temperature, unit: entityUnits.celsius }))
+  }
+  
+  for (let i = 0; i < cellCount.state(); ++i) {
+    cells.push({
+      voltage: dev.sensor('cell' + i + '_voltage', 'Zelle ' + (i + 1) + ' Spannung', {
+        category: entityCategories.diagnostic,
+        class: entityClasses.voltage,
+        unit: entityUnits.volt
+      }),
+      balance: dev.binarySensor('cell' + i + '_balance', 'Zelle ' + (i + 1) + ' Balance', {
+        category: entityCategories.diagnostic,
+      })
     })
-  }))
-
-  // static states
-  name.state((await read('name')).toString())
-  softwareVersion.state(info.softwareVersion)
-  manufactureDate.state(info.manufactureDate)
-  cellCount.state(info.packCells)
-  temperatureSensorCount.state(info.ntcCount)
+  }
 
   // dynamic states
   poll(1000, async () => {
-    const valuesInfo = decodeMessage('info', await read('info'))
-
-    chargeEnabled.state(valuesInfo.chargeEnabled ? stateValues.on : stateValues.off)
-    dischargeEnabled.state(valuesInfo.dischargeEnabled ? stateValues.on : stateValues.off)
-    voltage.state(filters.packVoltage.filter(valuesInfo.packVoltage))
-    current.state(filters.packCurrent.filter(valuesInfo.packCurrent))
-    stateOfCharge.state(valuesInfo.stateOfCharge)
-    designCapacity.state(valuesInfo.designCapacity)
-    cycleCapacity.state(valuesInfo.cycleCapacity)
-    cycleCount.state(valuesInfo.cycleCount)
-    cells.forEach((c, i) => c.balance.state(valuesInfo.balance[i] ? stateValues.on : stateValues.off))
-    temperatures.forEach((t, i) => t.state(valuesInfo.temperatures[i]))
-    errors.forEach((e, i) => e.state(valuesInfo.errors[i] ? stateValues.on : stateValues.off))
-
-    const valuesVoltages = decodeMessage('voltages', await read('voltages'))
-
-    cells.forEach((c, i) => c.voltage.state(valuesVoltages[i]))
+    await requestInfo()
+    await requestVoltages()
   })
 }
 
@@ -120,32 +148,4 @@ function checksum (payload) {
   const checksum = 0x10000 - payload.reduce((s, v) => s + v, 0)
 
   return Buffer.from([checksum >> 8, checksum & 0xFF])
-}
-
-function decodeMessage (type, buffer) {
-  switch (type) {
-    case 'info':
-      if (buffer.length < 23) throw new Error(ERROR_BUFFER_TOO_SHORT)
-
-      return {
-        packVoltage: buffer.readUInt16BE(0) / 100,
-        packCurrent: buffer.readInt16BE(2) / 100,
-        cycleCapacity: buffer.readUInt16BE(4)  / 100,
-        designCapacity: buffer.readUInt16BE(6) / 100,
-        cycleCount: buffer.readUInt16BE(8),
-        manufactureDate: (d => [(d >> 9) + 2000, ((d >> 5) & 15).toString(10).padStart(2, '0'), (d & 31).toString(10).padStart(2, '0')].join('-'))(buffer.readUInt16BE(10)),
-        balance: Array.from({ length: buffer.readUInt8(21) }).map((v, i) => !!(((i < 16 ? buffer.readUInt16BE(12) : buffer.readUInt16BE(14)) >> i%16) & 1)),
-        errors: Array.from({ length: 16 }).map((v, i) => !!((buffer.readUInt16BE(16) >> i) & 1)),
-        softwareVersion: '0x' + buffer.readUInt8(18).toString(16),
-        stateOfCharge: buffer.readUInt8(19),
-        chargeEnabled: buffer.readUInt8(20) & 1,
-        dischargeEnabled: buffer.readUInt8(20) >> 1,
-        packCells: buffer.readUInt8(21),
-        ntcCount: buffer.readUInt8(22),
-        temperatures: Array.from({ length: buffer.readUInt8(22) }).map((v, i) => (buffer.readUInt16BE(23 + (i * 2)) - 2731) / 10)
-      }
-
-    case 'voltages':
-      return Array.from({ length: buffer.length / 2 }).map((v, i) => buffer.readUInt16BE(i * 2) / 1000)
-  }
 }
