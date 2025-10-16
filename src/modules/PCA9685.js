3aee78a6bb6121447cc0eef4759844564cdb9aff
Robert Schröder <robert.schroeder@agila.de>
2025-10-16 16:44:27 +0200

    refactored pca9685 module

--- src/modules/PCA9685.js
+++ src/modules/PCA9685.js
@@ -1,8 +1,6 @@
 import i2c from 'i2c-bus'
 import PCA9685 from 'pca9685'
 
-const LIGHT_BRIGHTNESS_SCALE = 255
-
 export default async ({ device, prop, stateValues, log, logError }) => {
   const dev = device('NXP', 'PCA9685', '1')
   const connection = prop('connection', { bus: 1, address: 0x40 })
@@ -11,13 +9,11 @@ export default async ({ device, prop, stateValues, log, logError }) => {
     const channel = channels.find(c => i === c.id) || {}
     const data = { id: i }
     const id = 'channel' + i
-    const name = channel.name || 'Channel #' + channel.id
+    const name = channel.name || 'Channel #' + i
 
-   if (channel.type === 'switch') {
-      data.entity = dev.switch(id, name)
-      data.entity.state(stateValues.off)
-   } else if (channel.type === 'light') {
-      data.entity = dev.light(id, name, { brightness: true, brightnessScale: LIGHT_BRIGHTNESS_SCALE })
+    if (channel.type === 'light') {
+      data.scale = channel.scale || 1
+      data.entity = dev.light(id, name, data.scale > 1 ? { brightness: true, brightnessScale: data.scale } : { brightness: false })
     } else {
       data.min = channel.min || 0
       data.max = channel.max || 2600
@@ -34,26 +30,27 @@ export default async ({ device, prop, stateValues, log, logError }) => {
         i2c: i2c.openSync(connection.bus),
         address: connection.address,
         frequency: 120,
-        debug: true
+        debug: false
       }, error => error ? reject(error) : resolve(driver))
     }))()
 
     collection.forEach(({ id, min, max, scale, entity }) => {
-      if (entity.get('type') === 'switch') {
-        entity.command(value => {
-          controller.setDutyCycle(id, value === stateValues.on ? 1 : 0)
-          entity.state(value)
-        })
-      } else if (entity.get('type') === 'light') {
+      if (entity.get('type') === 'light') {
         entity.command(value => {
           value = JSON.parse(value)
-          
-          if (!('brightness' in value)) {
-            value.brightness = value.state === stateValues.on ? LIGHT_BRIGHTNESS_SCALE : 0
-          }
 
-          controller.setDutyCycle(id, value.brightness  / LIGHT_BRIGHTNESS_SCALE)
-          entity.state(value)
+          if (value.state === stateValues.off) {
+            controller.setDutyCycle(id, 0)
+            entity.state({ state: stateValues.off })
+          } else if (scale > 1) {
+            const brightness = value.brightness || scale
+
+            controller.setDutyCycle(id, brightness / scale)
+            entity.state({ state: stateValues.on, brightness })
+          } else {
+            controller.setDutyCycle(id, 1)
+            entity.state({ state: stateValues.on })
+          }
         })
       } else {
         entity.command(value => {
